51674e3a18975e1c08cdc2f07d3ff4220a8880b4
Praise Omogbemeh <onosithena@gmail.com>
2023-01-25 20:58:29 -0600

    Added the owners controller and owners view

--- pet-clinic/pet-clinic-webapp/src/main/java/icu/praise/petclinic/controllers/OwnerController.java
+++ pet-clinic/pet-clinic-webapp/src/main/java/icu/praise/petclinic/controllers/OwnerController.java
@@ -5,10 +5,11 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-@RequestMapping({"", "/", "index", "index.html"})
-public class IndexController {
+@RequestMapping("owner")
+public class OwnerController {
+
     @GetMapping("")
-    public String displayIndexPage() {
-        return "index";
+    public String displayOwnerPage() {
+        return "owner";
     }
 }
