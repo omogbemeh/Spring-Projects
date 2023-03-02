76a8bbf3c4d068b5d4b67baaaac6bda1aa0aece0
Praise Omogbemeh <onosithena@gmail.com>
2023-03-01 22:09:42 -0600

    Initial commit for the recipe application

---
+++ recipe-app/src/test/java/dev/praiseo/recipeapp/RecipeAppApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.praiseo.recipeapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecipeAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
