87a78412c2696367df816d288adae91df1dea2f7
Praise Omogbemeh <onosithena@gmail.com>
2023-03-03 22:56:01 -0600

    Added the index controller and view

---
+++ expense-tracker/expense-tracker-web/src/main/java/dev/praiseo/controllers/IndexController.java
@@ -0,0 +1,15 @@
+package dev.praiseo.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping({ "", "/", "index", "index.html"})
+public class IndexController {
+    @GetMapping("")
+    public String displayIndex() {
+        return "index";
+    }
+
+}
