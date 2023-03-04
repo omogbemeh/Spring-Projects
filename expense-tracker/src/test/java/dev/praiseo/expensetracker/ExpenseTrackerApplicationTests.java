9ba69903bb88ee1e5c0c980da9f580f07cda0e7f
Praise Omogbemeh <onosithena@gmail.com>
2023-03-03 22:46:08 -0600

    Added the expense tracker starter files

---
+++ expense-tracker/src/test/java/dev/praiseo/expensetracker/ExpenseTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.praiseo.expensetracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExpenseTrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
--- recipe-app/recipe-app-web/src/main/java/dev/praiseo/controllers/IndexController.java
+++ recipe-app/recipe-app-web/src/main/java/dev/praiseo/controllers/IndexController.java
@@ -10,6 +10,7 @@ public class IndexController {
 
     @GetMapping("")
     public String displayIndex() {
-        return "index";
+        return "index ";
     }
+
 }
